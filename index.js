5a8bc4aaaa8731a5d8bd12845f1985ba202d3387
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-01-30 21:48:11 -0500

    code added

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove("main")
+let newHeader=document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent="Madaline is the champion";
+document.body.append(newHeader)
